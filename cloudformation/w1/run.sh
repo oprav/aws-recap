3c34920a44316dd2a7377164324c9c2a2734c152
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2021-05-21 13:04:42 +0300

    add cloudformation script to spin up a single EC2 instance

---
+++ cloudformation/w1/run.sh
@@ -0,0 +1,2 @@
+aws cloudformation create-stack --stack-name mystack --template-body file://main.template
+# aws cloudformation create-stack --stack-name mystack --template-body file://main.template
