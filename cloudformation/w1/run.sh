b5006d643b84454fc2ff5258fc9d3378ae86d86f
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2021-05-28 10:24:41 +0300

    update dir structure

--- cloudformation/w1/run.sh
+++
@@ -1,2 +0,0 @@
-aws cloudformation create-stack --stack-name mystack --template-body file://main.template
-# aws cloudformation create-stack --stack-name mystack --template-body file://main.template
